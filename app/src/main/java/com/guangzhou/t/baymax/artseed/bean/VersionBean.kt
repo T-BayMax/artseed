edee8a7085c049d3dc20ce0e816d1f4381efb9eb
T-BayMax <1729450036@qq.com>
2019-02-12 10:59:35 +0800

    代码优化

---
+++ app/src/main/java/com/guangzhou/t/baymax/artseed/bean/VersionBean.kt
@@ -0,0 +1,10 @@
+package com.guangzhou.t.baymax.artseed.bean
+
+class VersionBean {
+    var content: String? = null
+    var id: String? = null
+    var time: String? = null
+    var title: String? = null
+    var url: String? = null
+    var version: String? = null
+}
